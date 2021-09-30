eec249891314a0bfc4eca9c886507e80b1de9b0f
c_door In <55236660+git-tutorial-cmd@users.noreply.github.com>
2021-10-01 01:34:32 +0300

    fixed by review from 20.09

--- main.py
+++ main.py
@@ -1,57 +1,72 @@
-from http.server import HTTPServer, SimpleHTTPRequestHandler
-from jinja2 import Environment, FileSystemLoader, select_autoescape
+import os
 import collections
-import pandas
 import datetime
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+import argparse
+import pandas
+from dotenv import load_dotenv
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
-
-template = env.get_template('template.html')
-
-def years_from_start():
-    start = datetime.datetime(year=1920, month=1, day=1)
-    now = datetime.datetime.now()
-    delta = now.year - start.year
-    return delta
-
-def set_year_word():
-    years_amount = str(years_from_start())
-    if years_amount[-2] != '1':
-        if years_amount[-1] == '1':
+def set_year_word(age):
+    if age[-2] != '1':
+        if age[-1] == '1':
             return 'год'
-        elif years_amount[-1] > 1 and years_amount[-1] < 5:
+        elif age[-1] > 1 and age[-1] < 5:
             return 'года'
     return 'лет'
 
+
 def get_wine_cards_from_excel(file):
-    wine_from_excel = pandas.read_excel(
+    wine_cards_from_excel = pandas.read_excel(
         io=file,
         na_values=None,
         keep_default_na=False,
     )
-    return wine_from_excel.to_dict(orient='records')
-
-wine_dict = get_wine_cards_from_excel('input/wine3.xlsx')
-
-def sort_by_categories(source_dict, column_name):
-    sort_by_categories = collections.defaultdict(list)
-    for card in source_dict:
-        sort_by_categories[card[column_name]].append(card)
-    return sort_by_categories
+    return wine_cards_from_excel.to_dict(orient='records')
 
 
-rendered_page = template.render(
-    years_old=years_from_start(),
-    year_word=set_year_word(),
-    sorted_wine_dict=sort_by_categories(wine_dict, column_name='Категория'),
-)
+def sort_by_categories(source_table, column_name):
+    categories = collections.defaultdict(list)
+    for card in source_table:
+        categories[card[column_name]].append(card)
+    return categories
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+def main():
+    load_dotenv()
+    source_folder_path = os.getenv('SOURCE_FOLDER_PATH')
+    xls_file_name = os.getenv('XLS_FILE_NAME')
+
+    parser = argparse.ArgumentParser(
+        description='Create index.html from template using data from wine excel table'
+    )
+    parser.add_argument('-src', '--source_file', default=xls_file_name)
+    args = parser.parse_args()
+    source_file = args.source_file
+
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+    template = env.get_template('template.html')
+
+    age = str(datetime.datetime.now().year - 1920)
+    wine_cards = get_wine_cards_from_excel(f'{source_folder_path}{source_file}')
+
+    rendered_page = template.render(
+        years_old=age,
+        year_word=set_year_word(age),
+        sorted_wine_cards=sort_by_categories(wine_cards, column_name='Категория'),
+    )
+
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
